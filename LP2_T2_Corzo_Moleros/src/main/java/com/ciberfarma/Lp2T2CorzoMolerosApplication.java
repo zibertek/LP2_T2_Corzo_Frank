6271de6e1ff36a1c8d4a651d18982a8573e01a9c
zibertek <i202411248@cibertec.edu.pe>
2025-06-21 11:10:21 -0500

    Proyecto iniciado

---
+++ LP2_T2_Corzo_Moleros/src/main/java/com/ciberfarma/Lp2T2CorzoMolerosApplication.java
@@ -0,0 +1,13 @@
+package com.ciberfarma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lp2T2CorzoMolerosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lp2T2CorzoMolerosApplication.class, args);
+	}
+
+}
---
+++ LP2_T2_Corzo_Moleros/src/test/java/com/ciberfarma/Lp2T2CorzoMolerosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciberfarma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2T2CorzoMolerosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
